82f2ec68f3286788ebd4877db4888c594e6ace05
Nane Kratzke <nkratzke@users.noreply.github.com>
2015-03-19 20:08:24 +0100

    Update start-database.sh

--- start-database.sh
+++ start-database.sh
@@ -9,8 +9,14 @@ file -bi /var/mysql/database.sql
 /usr/sbin/mysqld &
 sleep 5
 curl $url -o import.sql
+
+# Fixing some phpmysqladmin export problems
 sed -ri.bak 's/-- Database: (.*?)/CREATE DATABASE \1;\nUSE DATABASE \1;/g' import.sql
-if grep -q "CREATE DATABASE" import.sql; then :; else sed -ri.bak 's/-- MySQL dump/CREATE DATABASE `database_1`;\nUSE DATABASE `database_1`;\n-- MySQL dump/g' import.sql; fi
+
+# Fixing some mysqldump export problems (when run without --databases switch)
+# This is not tested so far
+# if grep -q "CREATE DATABASE" import.sql; then :; else sed -ri.bak 's/-- MySQL dump/CREATE DATABASE `database_1`;\nUSE DATABASE `database_1`;\n-- MySQL dump/g' import.sql; fi
+
 mysql --default-character-set=utf8 < import.sql
 rm import.sql
 mysqladmin shutdown
